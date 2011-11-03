eca46e84cda3d772831b9e4718d08136a3e8fd5c
Sergio Tanzilli <tanzilli@acmesystems.it>
2011-11-03 16:45:23 +0100

    gpio.py funzionante

--- gpio.py
+++ gpio.py
@@ -11,23 +11,46 @@ import os
 print "Test GPIO"
 
 test = { 
-	"J7" : [
+	"J6" : [
 		["3","4"],
-		["5","6"]
+		["5","6"],
+		["7","8"],
+		["9","10"],
+		["13","14"],
+		["15","16"],
+		["17","18"],
+		["19","20"],
+		["21","22"],
+		["24","36"],
+		["25","26"],
+		["27","28"],
+		["29","30"],
+		["31","32"],
+		["37","38"],
 	],
-#	"J6" : [
-#		[6,7],
-#		[8,9]
-#	],
+	"J7" : [
+		["3",  "4"],
+		["5",  "6"],
+		["7",  "8"],
+		["9", "10"],
+		["11","12"],
+		["13","14"],
+		["15","16"],
+		["17","18"],
+ 		["21","22"],
+ 		["31","32"],
+ 		["33","34"],
+ 		["35","36"],
+ 		["37","38"],
+ 
+	],
 }
 
 for connector_index, connector_item in enumerate(test):
 	#print connector_index, connector_item, test[connector_item]
 	for test_index, test_item in enumerate(test[connector_item]):
 		#for pin_index, pin_item in enumerate(test[connector_item][test_index]):
-		#print connector_item
-		#print test_item[0]
-		#print test_item[1]
+		print "Test %s.%s - %s.%s" % (connector_item,test_item[0],connector_item,test_item[1])
 
 		pin_out = fox.Pin(connector_item,test_item[0],'low')
 		pin_in = fox.Pin(connector_item,test_item[1],'in')
