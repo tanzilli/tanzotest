8c6143b5d735f7e44b36c39a063816137d8e9f9e
Sergio Tanzilli <tanzilli@acmesystems.it>
2011-12-01 17:12:34 +0100

    Modificato il README

--- README.md
+++ README.md
@@ -2,8 +2,3 @@ Programmi di test veloci per schede Acme
 
 * gpio.py Test pin su J6 e J7 della FOXG20 
 
-Per usare i programmi di test occorre installare il modulo fox.py con
-
-	debarm:~# wget http://raw.github.com/AcmeSystems/playground/master/python/fox/fox.py
-	debarm:~# wget http://raw.github.com/AcmeSystems/playground/master/python/fox/setup.py
-	debarm:~# python setup.py install
--- ft.py
+++ ft.py
@@ -1,30 +1,5 @@
 #!/usr/bin/python
 
-# flash.py 
-# versione per la produzione di pizzica.py
-
-# In Circuit Programming utility for FOX Board and Netus G20  
-#
-# Author: Sergio Tanzilli - tanzilli@acmesystems.it
-#
-# Copyright 2010 Acme Systems srl. Ladispoli (Rome) Italy
-# http://www.acmesystems.it
-# All Rights Reserved.
-# 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
 import serial
 import time
 import sys
