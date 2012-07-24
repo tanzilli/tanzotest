132bfb9b32cc9ef15c2e1fbd8123b417eeece7f8
Sergio Tanzilli <tanzilli@acmesystems.it>
2012-07-24 18:31:52 +0200

    Versione Aria funzionante con GPIO

--- aria_gpio.py
+++ aria_gpio.py
@@ -56,48 +56,61 @@ test = {
 
 for connector_index, connector_item in enumerate(test):
 	#print connector_index, connector_item, test[connector_item]
-		for test_index, test_item in enumerate(test[connector_item]):
-			#for pin_index, pin_item in enumerate(test[connector_item][test_index]):
-			#print "Test %s.%s - %s.%s" % (connector_item,test_item[0],connector_item,test_item[1])
 
-			while True:
-				error_counter=0
-				pin_out = acmeboards.Pin(connector_item,test_item[0],'low')
-				pin_in = acmeboards.Pin(connector_item,test_item[1],'in')
+	for test_index, test_item in enumerate(test[connector_item]):
+		#for pin_index, pin_item in enumerate(test[connector_item][test_index]):
+		#print "Test %s.%s - %s.%s" % (connector_item,test_item[0],connector_item,test_item[1])
 
-				if pin_in.get_value()<>0:
-					print "gpio error %s.%s = low ->  %s.%s" % (connector_item,test_item[0],connector_item,test_item[1])
-					error_counter = error_counter + 1
+		while True:
+			error_counter=0
 
-				pin_out = acmeboards.Pin(connector_item,test_item[0],'high')
-				pin_in = acmeboards.Pin(connector_item,test_item[1],'in')
+			#TEST A pin su anodo alto
+			#  Il pin sul catodo deve valere 1
 
-				if pin_in.get_value()<>1:
-					print "gpio error %s.%s = high ->  %s.%s" % (connector_item,test_item[0],connector_item,test_item[1])
-					error_counter = error_counter + 1
+			pin_out = acmeboards.Pin(connector_item,test_item[0],'high')
+			pin_in = acmeboards.Pin(connector_item,test_item[1],'in')
 
-				pin_out = acmeboards.Pin(connector_item,test_item[1],'low')
-				pin_in = acmeboards.Pin(connector_item,test_item[0],'in')
+			if pin_in.get_value()<>1:
+				print "TEST A: Pin %s.%s o %s.%s in corto verso massa" % (connector_item,test_item[0],connector_item,test_item[1])
+				error_counter = error_counter + 1
 
-				if pin_in.get_value()<>0:
-					print "gpio error %s.%s = low ->  %s.%s" % (connector_item,test_item[1],connector_item,test_item[0])
-					error_counter = error_counter + 1
+			#TEST B pin su anodo basso
+			#  Il pin sul catodo deve valere 1
 
-				pin_out = acmeboards.Pin(connector_item,test_item[1],'high')
-				pin_in = acmeboards.Pin(connector_item,test_item[0],'in')
+			pin_out = acmeboards.Pin(connector_item,test_item[0],'low')
+			pin_in = acmeboards.Pin(connector_item,test_item[1],'in')
 
-				if pin_in.get_value()<>1:
-					print "gpio error %s.%s = high ->  %s.%s" % (connector_item,test_item[1],connector_item,test_item[0])
-					error_counter = error_counter + 1
+			if pin_in.get_value()<>1:
+				print "TEST B: Pin %s.%s e %s.%s in corto" % (connector_item,test_item[0],connector_item,test_item[1])
+				error_counter = error_counter + 1
 
-				if error_counter==0:
-					break
-				else:
-					time.sleep(1)
+			#TEST C pin su catodo basso
+			#  Il pin sul anodo deve valere 0
+
+			pin_out = acmeboards.Pin(connector_item,test_item[1],'low')
+			pin_in = acmeboards.Pin(connector_item,test_item[0],'in')
+
+			if pin_in.get_value()<>0:
+				print "TEST C: Pin %s.%s o %s.%s staccati" % (connector_item,test_item[0],connector_item,test_item[1])
+				error_counter = error_counter + 1
+
+			#TEST D pin su catodo alto
+			#  Il pin sul anodo deve valere 1
+
+			pin_out = acmeboards.Pin(connector_item,test_item[1],'high')
+			pin_in = acmeboards.Pin(connector_item,test_item[0],'in')
+
+			if pin_in.get_value()<>1:
+				print "TEST D: Pin %s.%s o %s.%s in corto verso massa" % (connector_item,test_item[0],connector_item,test_item[1])
+				error_counter = error_counter + 1
+
+			if error_counter==0:
+				break
+			else:
+				time.sleep(1)
 
 print "GPIO test OK"
 #os.system("rm index.html");
 #os.system("wget http://10.55.99.2");
-
 
 
