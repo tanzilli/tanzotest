220f149e83a6d2c4116d0eb0abd0bb7f86fda0e0
Sergio Tanzilli <tanzilli@acmesystems.it>
2016-09-16 21:49:04 +0200

    Test cool project

--- coolproject/touch.py
+++ coolproject/touch.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
-# Banco di Test per NetusG20 e FOX Board G20
-# Software lato PC
-# Author: Sergio Tanzilli - tanzilli@acmesystems.it
+# Banco di Test per Multicon Touch
+# Lanciare su un PC Linux dotato di porta seriale di debug
 
 import serial
 import time
@@ -10,125 +9,85 @@ import sys
 import getopt
 import string 
 import datetime
+import select
+import termios
+import sys
+import tty
+
+def isData():
+	return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+
 
 color_warning = "\x1B[30;41m" 
 color_pass = "\x1B[30;42m" 
 color_normal = "\x1B[0m" 
 
-elenco_test = {
-	"AcmeBoot":"-",
-	"microSD":"-",
-	"sda1":"-",
-	"sdb1":"-",
-	"eth0":"-",
-	"login":"-",
-	"password":"-",
-	"gpio":"-",
-}
-
-
 serialdevice = "/dev/ttyUSB0"
 
-print "Banco di Test NetusG20"
-print "Version: 2.01"
+print "Banco di Test Multicon Touch"
 
 
 #Open the serial port 
 ser = serial.Serial(
 	port=serialdevice, 
 	baudrate=115200, 
-	timeout=1,
+	timeout=0.1,
 	parity=serial.PARITY_NONE,
 	stopbits=serial.STOPBITS_ONE,
 	bytesize=serial.EIGHTBITS
 )  
 ser.flushInput()
+old_settings = termios.tcgetattr(sys.stdin)
 
 print "Caratteri in ricezione dalla DebugPort (type Ctrl-C to exit)"
 
 token=""
-while True:
-	s = ser.read(1) 
-	sys.stdout.write(s)
-	sys.stdout.flush()
-	token += s
+try:
+	tty.setcbreak(sys.stdin.fileno())
 
-	pos = token.find("AcmeBoot")
-	if pos >= 0:
-		token=""
-		elenco_test["AcmeBoot"]="OK"
-		continue
- 
-	pos = token.find("Jump to Kernel")
-	if pos >= 0:
-		token=""
-		elenco_test["microSD"]="OK"
-		continue
+	while 1:
+		s = ser.read(1) 
+		sys.stdout.write(s)
+		sys.stdout.flush()
+		token += s
 
-	pos = token.find("sda1")
-	if pos >= 0:
-		token=""
-		elenco_test["sda1"]="OK"
-		continue
+		pos = token.find("acqua login:")
+		if pos >= 0:
+			token=""
+			ser.write("root\r")
+			continue
 
-	pos = token.find("sdb1")
-	if pos >= 0:
-		token=""
-		elenco_test["sdb1"]="OK"
-		continue
+		pos = token.find("Password:")
+		if pos >= 0:
+			token=""
+			ser.write("acmesystems\r")
+			continue
 
-	pos = token.find("debarm login:")
-	if pos >= 0:
-		token=""
-		elenco_test["login"]="OK"
-		ser.write("root\r")
-		continue
+		if isData():
+			c = sys.stdin.read(1)
+			if c == '\x1b':         # x1b is ESC
+				print "\n"
+				print color_pass + " Risultato finale dei test" + color_normal
+				print "\n"
+				ser.close()
+				break
+				
+			if c == "1":
+				ser.write("DISPLAY=\":0\" xinput_calibrator --geometry 480x272\r");
+				continue
+				
+			if c == "2":
+				ser.write("ping -c 4 www.acmesystems.it\r");
+				continue
 
-	pos = token.find("Password:")
-	if pos >= 0:
-		token=""
-		elenco_test["password"]="OK"
-		ser.write("netusg20\r")
-		time.sleep(0.5)
-		ser.write("./gpio.py\r")
-		continue
+			if c == "h":
+				ser.write("halt\r");
+				continue
 
-	pos = token.find("GPIO test OK")
-	if pos >= 0:
-		token=""
-		dataoracorrente=datetime.datetime.now().strftime("%m%d%H%M%Y")
-		comando =  "date " + dataoracorrente + "\r"
-		ser.write(comando)
+finally:
+	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
-		elenco_test["gpio"]="OK"
-		continue
 
-	pos = token.find("`index.html' saved")
-	if pos >= 0:
-		token=""
 
-		ser.write("halt\r")
-
-		elenco_test["eth0"]="OK"
-		continue
-
-	pos = token.find("Power down")
-	if pos >= 0:
-		token=""
-		break
-
-print "\n"
-print color_pass + " Risultato finale dei test" + color_normal
-print "\n"
-
-for test in elenco_test:
-	print test + " -> ",
-
-	if elenco_test[test]=="OK":
-		print elenco_test[test]
-	else:
-		print color_warning + "error" + color_normal
-
-ser.close()
 
 
